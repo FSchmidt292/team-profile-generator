6301710a49043b86f43203ca9fc469d367872086
Trey S <fjschmidt292@gmail.com>
2022-05-02 17:18:31 -0400

    tests for employee complete

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,4 +7,27 @@ test('creates an employee object', () => {
   expect(employee.id).toEqual(expect.any(Number));
   expect(employee.email).toBe('email@email.com');
   expect(employee.role).toBe('Employee');
-}) 
+});
+
+test('get employee id value', () => {
+    const employee = new Employee(1,'Trey Schmidt', 'email@email.com');
+    expect(employee.getId()).toEqual(expect.any(Number));
+})
+
+test('get employee name value', () => {
+    const employee = new Employee(1,'Trey Schmidt', 'email@email.com');
+
+    expect(employee.getName()).toBe('Trey Schmidt');
+});
+
+test('get employee email value', () => {
+    const employee = new Employee(1, 'Trey Schmidt', 'email@email.com');
+
+    expect(employee.getEmail()).toBe('email@email.com');
+});
+
+test('get employee role value', () => {
+    const employee = new Employee(1, 'Trey Schmidt', 'email@email.com');
+
+    expect(employee.getRole()).toEqual(expect.any(String));
+})
